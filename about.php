bafb929f5e90596d1ec5151b5e6924739cfabfa7
Hans Evan Tatipata <thansevan@gmail.com>
2022-06-01 00:17:15 +0700

    sisa 1 lagi admin dan poster (klo jadi)

--- about.php
+++ about.php
@@ -5,7 +5,43 @@ $data = array(
 );
 ?>
 <?php include 'include/header.php'; ?>
+
+<?php
+
+$events = mysqli_query($con, "SELECT COUNT(*) FROM events");
+$registrants = mysqli_query($con, "SELECT COUNT(*) FROM registrants");
+
+?>
+
 <div class="container p-5">
-    Halaman About
+    <!-- <p class="title">Halaman About</p> -->
+
+    <div class="row justify-content-center">
+        <div class="col-md-4 mt-3">
+            <div class="card text-center">
+                <div class="card-header">
+                    Webinar
+                </div>
+                <i class="bi bi-back" style="font-size: 4rem;"></i>
+            </div>
+        </div>
+        <div class="col-md-4 mt-3">
+            <div class="card text-center">
+                <div class="card-header">
+                    Pendaftar
+                </div>
+                <i class="bi bi-back" style="font-size: 4rem;"></i>
+            </div>
+        </div>
+        <div class="col-md-5 mt-md-5 mt-3">
+            <div class="card text-center">
+                <div class="card-header">
+                    Biaya Pendaftaran
+                </div>
+                <i class="bi bi-back" style="font-size: 4rem;"></i>
+            </div>
+        </div>
+    </div>
+
 </div>
 <?php include 'include/footer.php'; ?>
