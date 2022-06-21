5309b411308e50ce1650f9d08ed633bb00b9c425
Hans Evan Tatipata <thansevan@gmail.com>
2022-06-22 00:35:13 +0700

    New home page

--- index.php
+++ index.php
@@ -18,16 +18,18 @@ $registrants = mysqli_fetch_array(mysqli_query($con, "SELECT COUNT(*) FROM regis
     <div class="container my-5">
         <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg justify-content-between">
             <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-                <h1 class="display-4 fw-bold lh-1">Webinar di lingkungan FIKTI UG</h1>
-                <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                <h1 class="display-4 fw-bold lh-1">Website Pendaftaran Webinar FIKTI UG</h1>
+                <p class="lead">BEM FIKTI UG mempersembahkan macam-macam webinar dari program kerja tiap departemen atau biro kami yang akan memberikan ilmu dan wawasan yang bermanfaat kepada para peserta webinar. Webinar ini hanya untuk mahasiswa di lingkungan Fakultas Ilmu Komputer dan Teknologi Informasi dan <b>tidak dipungut biaya</b> untuk mendaftar semua acara yang tersedia disini.</p>
+                <!-- <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p> -->
                 <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
-                    <button type="button" class="btn btn-dark btn-lg px-4 me-md-2 fw-bold" data-aos="fade-up" data-aos-duration="500">Get Started!</button>
+                    <a href="webinars.php" class="btn btn-dark btn-lg px-4 me-md-2 fw-bold" data-aos="fade-up" data-aos-duration="500">Lihat Webinar</a>
+                    <a href="regist.php" class="btn btn-outline-dark btn-lg px-4 me-md-2 fw-bold" data-aos="fade-up" data-aos-duration="500">Daftar Sekarang !</a>
                     <!-- <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button> -->
                 </div>
             </div>
             <div class="col-md-5">
                 <div class="row justify-content-center">
-                    <div class="col-md-8 mt-3">
+                    <div class="col-md-11 mt-3">
                         <div class="card card-home text-center" data-aos="fade-right" data-aos-duration="1000">
 
                             <div class="icon">
@@ -40,7 +42,7 @@ $registrants = mysqli_fetch_array(mysqli_query($con, "SELECT COUNT(*) FROM regis
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-8 my-3">
+                    <div class="col-md-11 my-3">
                         <div class="card card-home text-center" data-aos="fade-left" data-aos-duration="2000">
                             <div class="icon">
                                 <i class="bi bi-people" style="font-size: 3.5rem; color:var(--bs-purple)"></i>
@@ -52,7 +54,7 @@ $registrants = mysqli_fetch_array(mysqli_query($con, "SELECT COUNT(*) FROM regis
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-8 mb-3">
+                    <div class="col-md-11 mb-5">
                         <div class="card card-home text-center" data-aos="fade-right" data-aos-duration="3000">
                             <div class="icon">
                                 <i class="bi bi-folder-check" style="font-size: 3.5rem; color:var(--bs-yellow)"></i>
