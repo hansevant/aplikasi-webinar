60269bbfeca5bc816c38cba7ddb6a6b7dd26cba3
Hans Evan Tatipata <thansevan@gmail.com>
2022-06-22 01:42:42 +0700

    update navbar

--- admin/index.php
+++ admin/index.php
@@ -53,9 +53,9 @@ if (isset($_POST['login'])) {
                 </a> -->
 
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="./../" class="nav-link px-3 text-white ">Home</a></li>
-                    <li><a href="../about.php" class="nav-link px-3 text-white">Events</a></li>
-                    <li><a href="../regist.php" class="nav-link px-3 text-white">Register</a></li>
+                    <li><a href="./../" class="nav-link px-3 text-white ">Beranda</a></li>
+                    <li><a href="../about.php" class="nav-link px-3 text-white">Webinar</a></li>
+                    <li><a href="../regist.php" class="nav-link px-3 text-white">Pendaftaran</a></li>
                 </ul>
 
                 <!-- <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
