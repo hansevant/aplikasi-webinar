87b4e0d434b8ea3e740cafda4ad54a7b3a89404a
Hans Evan Tatipata <thansevan@gmail.com>
2022-06-23 01:55:37 +0700

    new database

--- db/webinar.sql
+++ db/webinar.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.7
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Jun 20, 2022 at 01:00 AM
--- Server version: 10.3.35-MariaDB
--- PHP Version: 7.4.29
+-- Host: 127.0.0.1
+-- Generation Time: Jun 22, 2022 at 08:54 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `technof4_webinar`
+-- Database: `webinar`
 --
 
 -- --------------------------------------------------------
@@ -35,6 +34,7 @@ CREATE TABLE `events` (
   `deskripsi` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` int(1) NOT NULL DEFAULT 0 COMMENT '1 soft delete, 0 save',
   `statusnya` int(1) NOT NULL DEFAULT 0
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
