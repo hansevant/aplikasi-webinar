8996016ba5c402a5fc8adb91d3663510f6b2a1b4
Hans Evan Tatipata <thansevan@gmail.com>
2022-06-25 00:45:19 +0700

    update UI

--- include/header.php
+++ include/header.php
@@ -20,27 +20,11 @@
     <header class="p-2 bg-dark text-white">
         <div class="container">
             <div class="flex-wrap p-1 align-items-center justify-content-center">
-                <!-- <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-                        <use xlink:href="#bootstrap"></use>
-                    </svg>
-                </a> -->
-
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li><a href="./" class="nav-link px-3 text-white <?= $data['active'] === 'home' ? 'fw-bold' : 'fw-none'; ?>">Beranda</a></li>
                     <li><a href="webinars.php" class="nav-link px-3 text-white <?= $data['active'] === 'webinar' ? 'fw-bold' : 'fw-none'; ?>">Webinar</a></li>
-                    <!-- <li><a href="regist.php" class="nav-link px-3 text-dark bg-white rounded-5">Register</a></li> -->
                     <li><a href="regist.php" class="nav-link px-3 text-white <?= $data['active'] === 'register' ? 'fw-bold' : 'fw-none'; ?>">Pendaftaran</a></li>
                 </ul>
-
-                <!-- <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                    <input type="search" class="form-control form-control-dark text-white bg-dark" placeholder="Search..." aria-label="Search">
-                </form> -->
-
-                <!-- <div class="text-end">
-                    <button type="button" class="btn btn-outline-light me-2">Login</button>
-                    <button type="button" class="btn btn-warning">Sign-up</button>
-                </div> -->
             </div>
         </div>
     </header>
