3e34a4dee89aa4356a7a1853337d450ff3f08d24
Hans Evan Tatipata <thansevan@gmail.com>
2022-06-25 01:13:52 +0700

    justifycontentcenter on webinar pages

--- webinars.php
+++ webinars.php
@@ -13,22 +13,18 @@ $queries = mysqli_query($con, "SELECT * FROM events WHERE statusnya = 1");
 ?>
 
 <div class="container">
-    <div class="row p-4">
+    <div class="row p-4 justify-content-center">
         <?php
         while ($data = mysqli_fetch_array($queries)) {
-            // $desc = mb_strimwidth($data['deskripsi'], 0, 200, "...");
             $foto = $data['foto'];
         ?>
             <div class="col-md-4 mb-3">
                 <div class="card cardz" style="background:url('assets/img/events/<?= $foto  ?>'); background-position:center;background-size: cover;">
-                    <!-- <div class="card-header">
-                    </div> -->
+
                     <div class="overlay">
                         <div class="p-2">
                             <a href="webinar.php?nama=<?= $data['id_webinar'] ?>" style="text-decoration: none;">
                                 <h2 class="card-title text-center text-light mt-5"><?= $data['nama']; ?></h2>
-
-                                <!-- Selengkapnya.. -->
                             </a>
                         </div>
                     </div>
