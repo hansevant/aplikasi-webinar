e4c056c15fe8d59e33b912a86519de8bdc1b0e5c
Hans Evan Tatipata <thansevan@gmail.com>
2022-06-25 00:45:37 +0700

    update UI

--- index.php
+++ index.php
@@ -18,13 +18,11 @@ $registrants = mysqli_fetch_array(mysqli_query($con, "SELECT COUNT(*) FROM regis
     <div class="container my-5">
         <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg justify-content-between">
             <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-                <h1 class="display-4 fw-bold lh-1">Website Pendaftaran Webinar FIKTI UG</h1>
-                <p class="lead">BEM FIKTI UG mempersembahkan macam-macam webinar dari program kerja tiap departemen atau biro kami yang akan memberikan ilmu dan wawasan yang bermanfaat kepada para peserta webinar. Webinar ini hanya untuk mahasiswa di lingkungan Fakultas Ilmu Komputer dan Teknologi Informasi dan <b>tidak dipungut biaya</b> untuk mendaftar semua acara yang tersedia disini.</p>
-                <!-- <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p> -->
+                <h1 class="display-4 fw-bold lh-1 judul mb-3">Pendaftaran Webinar Di Lingkungan FIKTI UG</h1>
+                <p class="lead mb-3">BEM FIKTI UG mempersembahkan macam-macam webinar dari program kerja tiap departemen atau biro kami yang akan memberikan ilmu dan wawasan yang bermanfaat kepada para peserta webinar. Webinar ini hanya untuk mahasiswa di lingkungan Fakultas Ilmu Komputer dan Teknologi Informasi dan <b>tidak dipungut biaya</b> untuk mendaftar semua acara yang tersedia disini.</p>
                 <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
                     <a href="webinars.php" class="btn btn-dark btn-lg px-4 me-md-2 fw-bold" data-aos="fade-up" data-aos-duration="500">Lihat Webinar</a>
                     <a href="regist.php" class="btn btn-outline-dark btn-lg px-4 me-md-2 fw-bold" data-aos="fade-up" data-aos-duration="500">Daftar Sekarang !</a>
-                    <!-- <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button> -->
                 </div>
             </div>
             <div class="col-md-5">
